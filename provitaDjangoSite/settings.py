5bc76efa520a8f5b083f5028258acee71208b9a4
Gary Zlobinskiy <garzlo22@bergen.org>
2020-02-22 21:01:03 -0500

    added MySQL server to provita django site

--- provitaDjangoSite/settings.py
+++ provitaDjangoSite/settings.py
@@ -77,7 +77,11 @@ WSGI_APPLICATION = 'provitaDjangoSite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'provitaschema',
+        'USER': 'dbadmin',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
